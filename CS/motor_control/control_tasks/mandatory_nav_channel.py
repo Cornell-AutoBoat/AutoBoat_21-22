3e87f0742e0d618322de59c9587534ba3450ab88
Amelia Kovacs <ajk296@cornell.edu>
2022-05-01 15:33:33 -0400

    MNC

--- CS/motor_control/control_tasks/mandatory_nav_channel.py
+++ CS/motor_control/control_tasks/mandatory_nav_channel.py
@@ -8,11 +8,25 @@ import numpy as np
 objects = np.array([])  # array of buoy objects from object_detection
 
 
+def pivot():
+    # if less than 2 valid buoys in sight at beginning of task, pivot until 2 are seen
+
+    # stop motors
+    while(len(filter(objects) < 2)):
+        # pivot in 1 circle
+        pass
+    if (len(filter(objects) < 2)):
+        return 0
+    return 1
+
+
 def filter(objects):
+    # filter buoys array to contain 2 objects: must be red or green and within a distance threshold
+
     threshold = 0  # distance of buoys to pay attention to, 120ft?
     buoys = objects[np.where(
-        objects.string == "green buoy" or objects.string == "red buoy")]
-    close_buoys = np.sort(buoys[np.where(objects.z < threshold)], order='oz')
+        objects.label == "green buoy" or objects.label == "red buoy")]
+    close_buoys = np.sort(buoys[np.where(objects.oz < threshold)], order='oz')
     if (len(close_buoys) > 2):      # gets nearest 2 buoys, objects is sorted by z value
         return close_buoys[:2]
     return close_buoys
@@ -27,15 +41,18 @@ def execute():
     # move forward to keep x vectors of all buoys within threshold
     # move until no buoys in sight (+ x sec)
 
+    if (pivot(objects) == 0):
+        finish()
+
     while (not isComplete()):
 
         buoys = filter(objects)
         if (buoys[0].ox < 0):
-            x1 = np.abs(buoys[0].ox)    # left buoy
-            x2 = np.abs(buoys[1].ox)    # right buoy
+            x1 = np.abs(buoys[0].ox)    # left (red) buoy
+            x2 = np.abs(buoys[1].ox)    # right (green) buoy
         else:
-            x1 = np.abs(buoys[1].ox)    # left buoy
-            x2 = np.abs(buoys[0].ox)    # right buoy
+            x1 = np.abs(buoys[1].ox)    # left (red) buoy
+            x2 = np.abs(buoys[0].ox)    # right (green) buoy
 
         threshold = 0
         if (np.abs(x1 - x2) < threshold):
@@ -47,9 +64,13 @@ def execute():
         else:
             # adjust by running left motor
             pass
+    finish()
+
+
+def finish():
+    # mark task as complete
 
     # move forward for x seconds
-
     SFR.MNC = True
     SFR.task = Task.DETERMINE_TASK
 
@@ -57,4 +78,5 @@ def execute():
 def isComplete():
     # returns a boolean indicating whether the mandatory navigation channel task is complete
     # complete when less than 2 "valid" buoys in sight
+
     return len(filter(objects)) < 2
