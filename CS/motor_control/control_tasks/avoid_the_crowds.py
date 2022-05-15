7c94c123682df04ad94cc64122e223e83a04d6a4
Henrygao19 <35242447+Henrygao19@users.noreply.github.com>
2022-05-15 16:14:19 -0400

    Update avoid_the_crowds.py
    comments

--- CS/motor_control/control_tasks/avoid_the_crowds.py
+++ CS/motor_control/control_tasks/avoid_the_crowds.py
@@ -15,6 +15,28 @@ def execute():
         SFR.AVOID_THE_CROWD = True
         SFR.task = Task.DETERMINE_TASK
 
+def configurationOne():
+    # The only buoy directly infront of it is a green and red
+    # choose the midpoint between the two buoy 
+    # make the boat go throug hthat midpoint 
+    pass
+def configurationTwo(): 
+    # There is a yellow buoy, green buoy, and red buoy directly in front of it 
+    # The yellow buoy must be relatively close to the green and red in terms of z distances
+    # find the midpt between the green and yellow and red and yellow
+    # prioritize based on how easy it will be to pass and then the distance to each
+    pass
+def configurationThree():
+    # there is yellow buoy, green buoy, and red buoy directly in front of it 
+    # the yellow buoy is beyond the threshold of the z distance allocated for configuration 2
+    # therefore, the boat will fix the midpoint distance between the red and green buoy
+    # if it sees the yellow buoy in front of it such that it will crash into it
+    # the room for error for the buoy being in front is another buoy (at the very least) to ensure no collisions occur
+    # the boat will make a fixed semicircle (pick side with the most leeway) around the yellow buoy
+     pass
+
+
+
 
 def isComplete():
     #returns a boolean indicating whether the avoid the crowds task is complete
